f3ea12c06aad84131eeac4dba96b6e3f815da7fe
Aryan Dwivedi <aryandwivedi352@gmail.com>
2026-01-18 00:44:28 -0600

    Remove Base44 dependency (static mode)

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
-import base44 from "@base44/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  logLevel: "error",
   base: "/my-portfolio/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
-      hmrNotifier: true,
-      navigationNotifier: true,
-      visualEditAgent: true,
-    }),
-    react(),
-  ],
+  plugins: [react()],
 });
