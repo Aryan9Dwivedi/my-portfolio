e236ff672609c972474906788bc433406643f03b
Aryan Dwivedi <aryandwivedi352@gmail.com>
2026-01-18 00:20:30 -0600

    Update Vite alias config for CI

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import base44 from "@base44/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   logLevel: "error",
   base: "/my-portfolio/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   plugins: [
